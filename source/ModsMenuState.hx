7fc74d5e91a06c80234ff0ee67329bebaa7d179d
Dxgamer <95672691+Amogus124@users.noreply.github.com>
2022-04-07 05:23:17 -0300

    update ModsMenuState.hx

--- source/ModsMenuState.hx
+++ source/ModsMenuState.hx
@@ -89,7 +89,7 @@ class ModsMenuState extends MusicBeatState
 		noModsTxt.screenCenter();
 		visibleWhenNoMods.push(noModsTxt);
 
-		var path:String = Paths.getPreloadPath 'modsList.txt';
+		var path:String = 'modsList.txt';
 		if(FileSystem.exists(path))
 		{
 			var leMods:Array<String> = CoolUtil.coolTextFile(path);
@@ -108,7 +108,7 @@ class ModsMenuState extends MusicBeatState
 
 		// FIND MOD FOLDERS
 		var boolshit = true;
-		if (FileSystem.exists(Paths.getPreloadPath "modsList.txt")){
+		if (FileSystem.exists("modsList.txt")){
 			for (folder in Paths.getModDirectories())
 			{
 				if(!Paths.ignoreModFolders.contains(folder))
@@ -460,7 +460,7 @@ class ModsMenuState extends MusicBeatState
 			fileStr += values[0] + '|' + (values[1] ? '1' : '0');
 		}
 
-		var path:String = Paths.getPreloadPath 'modsList.txt';
+		var path:String = 'modsList.txt';
 		File.saveContent(path, fileStr);
 	}
 
